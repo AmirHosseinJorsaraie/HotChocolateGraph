e153d9f1a3a15c0ab8203c91b1d36901078e04a1
HOME <HOME@DESKTOP-701C2VG>
2025-02-28 21:07:56 +0330

    [Fix] Change PersonMutation Names To Seprate From CourseMutation

--- HotChocolateGraph/Mutation/PersonMutation.cs
+++ HotChocolateGraph/Mutation/PersonMutation.cs
@@ -8,13 +8,13 @@ namespace HotChocolateGraph.Mutation
     [ExtendObjectType("Mutation")]
     public class PersonMutation
     {
-        public async Task<bool> Add([Service] IPersonRepository repository,
+        public async Task<bool> AddPerson([Service] IPersonRepository repository,
             Person dto, [Service] ITopicEventSender eventSender, CancellationToken cancellationToken)
         {
             try
             {
                 repository.Add(dto);
-                await eventSender.SendAsync($"{nameof(Add)}_{nameof(PersonMutation)}", dto, cancellationToken);
+                await eventSender.SendAsync($"{nameof(AddPerson)}_{nameof(PersonMutation)}", dto, cancellationToken);
                 return true;
             }
             catch (GraphQLException e)
@@ -24,14 +24,13 @@ namespace HotChocolateGraph.Mutation
 
         }
 
-        public async Task<bool> Update([Service] IPersonRepository repository, Person dto,
+        public async Task<bool> UpdatePereson([Service] IPersonRepository repository, Person dto,
             [Service] ITopicEventSender eventSender, CancellationToken cancellationToken)
         {
             try
             {
-                ConcurrentBag<Person> persons = new ConcurrentBag<Person>();
                 repository.Update(dto);
-                await eventSender.SendAsync($"{nameof(Update)}_{nameof(PersonMutation)}",dto, cancellationToken);
+                await eventSender.SendAsync($"{nameof(UpdatePereson)}_{nameof(PersonMutation)}",dto, cancellationToken);
                 return true;
             }
             catch (GraphQLException e)
