036aa528d40b23165166650a2fe33bfbc90eb215
HOME <HOME@DESKTOP-701C2VG>
2025-02-28 21:25:03 +0330

    [Feature] Add Course Subscription

---
+++ HotChocolateGraph/Subscription/CourseSubscription.cs
@@ -0,0 +1,17 @@
+﻿using HotChocolateGraph.Dto;
+using HotChocolateGraph.Mutation;
+
+namespace HotChocolateGraph.Subscription
+{
+    [ExtendObjectType("Subscription")]
+    public class CourseSubscription
+    {
+        [Subscribe]
+        [Topic($"{nameof(CourseMutation.AddCourse)}_{nameof(CourseMutation)}")]
+        public Course OnAddCourse([EventMessage] Course course) => course;
+        
+        [Subscribe]
+        [Topic($"{nameof(CourseMutation.UpdateCourse)}_{nameof(CourseMutation)}")]
+        public Course OnUpdateCourse([EventMessage] Course course) => course;
+    }
+}
